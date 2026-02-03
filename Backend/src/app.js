35817ed5c151babb9acf1e9f88f13085f77a327c
Prakashom18 <shahomprakash097@gmail.com>
2026-02-03 19:58:27 +0545

    adding encryption

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
+
 const authController = require('../src/controller/authController')
 app.get('/',(req,res)=>{
     res.send('Homepage');
