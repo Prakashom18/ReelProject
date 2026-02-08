60f8f5928661e63e99ff34427288a83c223c59d2
Prakashom18 <shahomprakash097@gmail.com>
2026-02-08 09:31:56 +0545

    creating middleware

--- Backend/src/midlleware/authMiddleware.js
+++ Backend/src/midlleware/authMiddleware.js
@@ -1,7 +1,31 @@
 const foodPartnerModel = require('../models/foodpartnerModel');
 const jwt = require('jsonwebtoken');
 
-async function authFoodPartnerMiddlewate(req,res,next){
+async function authFoodPartnerMiddleware(req,res,next){
     
+    const token = req.cookies.token;
+    if(!token){
+        return res.status(401).json({
+            message : "Please login first"
+        })
+    }
+    try {
+        const decoded = jwt.verify(token,process.env.JWt_SECRET);
+        
+        const foodPartner = await foodPartnerModel.findById(decoded.id);
+        req.foodPartner = foodPartner 
+        next();
+    }
+    catch(err){
 
+        return res.status(401).json({
+            message : "Invalid token"
+        })
+
+    }
+
+}
+
+module.exports = {
+    authFoodPartnerMiddleware
 }
