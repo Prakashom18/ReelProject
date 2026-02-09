51fafebabd1666b2f88a536829f43c0b823b87d4
Prakashom18 <shahomprakash097@gmail.com>
2026-02-09 22:46:44 +0545

    adding multer

--- Backend/src/routes/foodRoutes.js
+++ Backend/src/routes/foodRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const foodController = require('../controller/foodController');
 const authMiddleware = require('../midlleware/authMiddleware');
-
+const multer = require('multer');
 router.post('/',authMiddleware.authFoodPartnerMiddleware,foodController.createFood);
 
 
