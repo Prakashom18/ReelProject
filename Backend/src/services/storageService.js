63b001588a70b4aab1caf3011274ed90d4f90545
Prakashom18 <shahomprakash097@gmail.com>
2026-02-10 23:12:04 +0545

    adding storage services

---
+++ Backend/src/services/storageService.js
@@ -0,0 +1,9 @@
+// SDK initialization
+
+const ImageKit = require("imagekit");
+
+const imagekit = new ImageKit({
+    publicKey : "public_N0gRgKOD0pXLYqw9RgEuhxEEsiQ=",
+    privateKey : "private_ImV/uQiBpO******************",
+    urlEndpoint : "https://ik.imagekit.io/cl0gfau08"
+});
